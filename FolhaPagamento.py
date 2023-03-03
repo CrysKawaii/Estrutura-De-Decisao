b2f659c4a0263e88c8ae95d3296861ec2e1fbf3b
CrysKawaii <cryskawaiii@gmail.com>
2023-03-03 08:33:24 -0300

    Mostra uma tabela da folha de pagamento de salários

---
+++ FolhaPagamento.py
@@ -0,0 +1,27 @@
+ear_hour = float(input('Digite quanto você ganha por hora: R$'))
+hour_month = float(input('Digite quantas horas trabalha por mês: '))
+wage = ear_hour * hour_month
+ir = inss = fgts = 0
+if wage <= 900:
+    inss = wage * 10/100
+    fgts = wage * 11/100
+elif wage <= 1500:
+    ir = wage * 5/100
+    inss = wage * 10 / 100
+    fgts = wage * 11 / 100
+elif wage <= 2500:
+    ir = wage * 10/100
+    inss = wage * 10 / 100
+    fgts = wage * 11 / 100
+elif wage > 2500:
+    ir = wage * 20/100
+    inss = wage * 10 / 100
+    fgts = wage * 11 / 100
+
+print(f'''
+Salário Bruto         :  R${wage:.2f}
+(-) IR                :  R${ir:.2f}
+(-) INSS              :  R${inss:.2f}
+FGTS                  :  R${fgts:.2f}
+Total de descontos    :  R${ir + inss:.2f}
+Salário líquido       :  R${wage - ir - inss:.2f}''')
