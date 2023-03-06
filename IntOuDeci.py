b00ceb2db9b71b2c23bcdb1eaa446b7c9eecbfa3
CrysKawaii <cryskawaiii@gmail.com>
2023-03-06 15:18:36 -0300

    Informa se o número é inteiro ou decinal

---
+++ IntOuDeci.py
@@ -0,0 +1,8 @@
+from math import *
+num = str(input('Digite um número: '))
+if '.' in num:
+    print('Esse número é decimal!')
+elif ',' in num:
+    print('Esse número é decimal!')
+else:
+    print('Esse número é inteiro!')
